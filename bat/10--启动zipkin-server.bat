872f0b824b9f615d7b97d3158877c313b89955d6
rengang66 <rengang66@sina.com>
2019-10-13 18:24:27 +0800

    version 1.4

---
+++ bat/10--启动zipkin-server.bat
@@ -0,0 +1,3 @@
+@echo off
+set curdir=%~dp0
+java -Dfile.encoding=utf-8 -jar  zipkin-server-1.5.2.RELEASE.jar
---
+++ bat/20--启动springboot-zipkin0.bat
@@ -0,0 +1,3 @@
+@echo off
+set curdir=%~dp0
+java -Dfile.encoding=utf-8 -jar  springboot-zipkin0-0.0.1-SNAPSHOT.jar
